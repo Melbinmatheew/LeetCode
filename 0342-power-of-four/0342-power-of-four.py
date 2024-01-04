a4de226115a7abbd5fec566580ab321509b2c418
Melbin Mathew <130973266+Melbinmatheew@users.noreply.github.com>
2024-01-04 09:56:28 +0530

    Time: 36 ms (76.34%), Space: 17.4 MB (5.82%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        for i in range(20):
+            power=4**i
+            if power ==n:
+                return True
+            if power >n:
+                return False
